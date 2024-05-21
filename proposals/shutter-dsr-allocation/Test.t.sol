0e18ca3edf90174a6fcb4eee15edac14ae702de8
0xneves <asguilift@hotmail.com>
2024-05-21 22:24:42 +0700

    fix: added DaiJoin

--- proposals/shutter-dsr-allocation/Test.t.sol
+++ proposals/shutter-dsr-allocation/Test.t.sol
@@ -4,17 +4,17 @@ pragma solidity >=0.8.25 <0.9.0;
 import { Test } from "forge-std/src/Test.sol";
 import { console2 } from "forge-std/src/console2.sol";
 
-// import interfaces
-//  governance contract Shutter
 //  ERC20 (USDC, DAI)
 //      0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48
 //      0x6B175474E89094C44Da98b954EedeAC495271d0F
-//  Maker PSM
+//  Maker PSM (DssPsm, AuthGemJoin5)
 //      0x89B78CfA322F6C5dE0aBcEecab66Aee45393cC5A
 //      0x0A59649758aa4d66E25f08Dd01271e891fe52199
-//  Maker DSR (Pot, Vat)
+//  Maker DSR (DaiJoin,Pot, Vat, Vow)
+//      0x9759A6Ac90977b93B58547b4A71c78317f391A28
 //      0x197E90f9FAD81970bA7976f33CbD77088E5D7cf7
 //      0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B
+//      0xA950524441892A31ebddF91d3cEEFa04Bf454466
 
 contract Calldata is Test {
   /// @dev Top #1 USDC Holder will be impersonated
@@ -34,6 +34,7 @@ contract Calldata is Test {
   address AuthGemJoin5 = 0x0A59649758aa4d66E25f08Dd01271e891fe52199;
 
   /// @dev Maker DSR contract
+  address DaiJoin = 0x9759A6Ac90977b93B58547b4A71c78317f391A28;
   address Pot = 0x197E90f9FAD81970bA7976f33CbD77088E5D7cf7;
   address Vat = 0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B;
   address Vow = 0xA950524441892A31ebddF91d3cEEFa04Bf454466;
@@ -44,7 +45,7 @@ contract Calldata is Test {
   }
 
   /// @dev Basic test. Run it with `yarn test:fork` to see the console log.
-  function test_convertToDAI() external {
+  function test_depositUSDCtoPot() external {
     // Approve PSM to spend USDC {ERC20-approve}
     bytes memory approve = abi.encodeWithSelector(0x095ea7b3, AuthGemJoin5, amount * decimalsUSDC);
     (bool success, ) = USDC.call(approve);
@@ -64,13 +65,16 @@ contract Calldata is Test {
 
     // Assert increased balance of DAI
     assertEq(balanceAfter, amount * decimalsDAI);
-  }
 
-  function test_dripDAI() external {
     // Approve Pot to spend DAI {ERC20-approve}
-    bytes memory approve = abi.encodeWithSelector(0x095ea7b3, Pot, amount * decimalsDAI);
-    (bool success, ) = DAI.call(approve);
+    approve = abi.encodeWithSelector(0x095ea7b3, DaiJoin, amount * decimalsDAI);
+    (success, ) = DAI.call(approve);
     require(success, "DAI.approve failed");
+
+    // Join DAI to Vat {SdrManager-join}
+    bytes memory join = abi.encodeWithSelector(0x3b4da69f, Alice, amount * decimalsDAI);
+    (success, ) = DaiJoin.call(join);
+    require(success, "DaiJoin.join failed");
 
     // Hope to join Vat {Vat-hope}
     bytes memory hope = abi.encodeWithSelector(0xa3b22fc4, Pot);
@@ -79,14 +83,20 @@ contract Calldata is Test {
 
     // Drip DAI {Pot-drip}
     bytes memory drip = abi.encodeWithSelector(0x9f678cca);
-    bytes memory response;
     (success, response) = Pot.call(drip);
     require(success, "Pot.drip failed");
     uint256 chi = abi.decode(response, (uint256));
 
+    // Dai balance {Vat-dai}
+    bytes memory dai = abi.encodeWithSignature("dai(address)", Alice);
+    (success, response) = Vat.staticcall(dai);
+    require(success, "Vat.dai failed");
+    uint256 daiBalance = abi.decode(response, (uint256));
+    console2.log("daiBalance", daiBalance);
+
     // Join with DAI {Pot-join}
     uint256 RAY = 10 ** 27;
-    bytes memory join = abi.encodeWithSelector(0x049878f3, mul(amount, RAY) / chi);
+    join = abi.encodeWithSelector(0x049878f3, mul(amount, RAY) / chi);
     (success, ) = Pot.call(join);
     require(success, "Pot.join failed");
   }
