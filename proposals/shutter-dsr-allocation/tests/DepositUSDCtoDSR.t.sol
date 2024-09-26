7f70d18e58c5587d484b68f5b65532321ccad7e3
Alexandro T. Netto <alex.t.netto@gmail.com>
2024-09-26 15:45:29 -0300

    chore: linting

--- proposals/shutter-dsr-allocation/tests/DepositUSDCtoDSR.t.sol
+++ proposals/shutter-dsr-allocation/tests/DepositUSDCtoDSR.t.sol
@@ -10,45 +10,45 @@ import "../../../contracts/dai/interfaces/ISavingsDai.sol";
 import "../Context.sol";
 
 contract DepositUSDCtoDSR is Test, Context {
-  /// @dev A function invoked before each test case is run.
-  function setUp() public virtual {}
+    /// @dev A function invoked before each test case is run.
+    function setUp() public virtual { }
 
-  /**
-   * @dev Tests depositing USDC to the DSR contract.
-   *
-   * The test will:
-   * 1. Approve the PSM to spend USDC.
-   * 2. Convert USDC to DAI.
-   * 3. Approve the SavingsDai to spend DAI.
-   * 4. Deposit DAI in the SavingsDai contract.
-   */
-  function test_dsrAllocation_depositUSDCToDSR() external {
-    // Start pranking with the USDC owner
-    vm.startPrank(ShutterGnosis);
-    // Stores the previous balance of the user
-    uint256 initialGnosisUSDCBalance = USDC.balanceOf(ShutterGnosis);
-    // Approve PSM to spend USDC {ERC20-approve}
-    USDC.approve(AuthGemJoin5, amount * decimalsUSDC);
-    // Check if allowance is set for USDC {ERC20-allowance}
-    assert(USDC.allowance(ShutterGnosis, AuthGemJoin5) == amount * decimalsUSDC);
-    // Convert USDC to DAI {DssPsm-sellGem}
-    DssPsm.sellGem(ShutterGnosis, amount * decimalsUSDC);
-    // Check if DAI balance was increased {ERC20-balanceOf}
-    assert(DAI.balanceOf(ShutterGnosis) == amount * decimalsDAI);
-    assert(USDC.balanceOf(ShutterGnosis) == initialGnosisUSDCBalance - amount * decimalsUSDC);
-    // Stores the previous balance of the user
-    uint256 balanceOfSavingsDaiBefore = SavingsDai.balanceOf(ShutterGnosis);
-    // Approve SavingsDai to spend DAI {ERC20-approve}
-    DAI.approve(address(SavingsDai), amount * decimalsDAI);
-    // Check if allowance is set for DAI {ERC20-allowance}
-    assert(DAI.allowance(ShutterGnosis, address(SavingsDai)) == amount * decimalsDAI);
-    // Preview the amount of shares that will be received {SavingsDai-previewDeposit}
-    uint256 sharesToBeReceived = SavingsDai.previewDeposit(amount * decimalsDAI);
-    // Deposit DAI to SavingsDai {SavingsDai-deposit}
-    uint256 sharesReceived = SavingsDai.deposit(amount * decimalsDAI, ShutterGnosis);
-    // Check if the amount of shares received is the same as the previewed amount {SavingsDai-deposit}
-    assert(sharesReceived == sharesToBeReceived);
-    // Check if the user's balance of shares was increased {SavingsDai-balanceOf}
-    assert(sharesReceived == SavingsDai.balanceOf(ShutterGnosis) - balanceOfSavingsDaiBefore);
-  }
+    /**
+     * @dev Tests depositing USDC to the DSR contract.
+     *
+     * The test will:
+     * 1. Approve the PSM to spend USDC.
+     * 2. Convert USDC to DAI.
+     * 3. Approve the SavingsDai to spend DAI.
+     * 4. Deposit DAI in the SavingsDai contract.
+     */
+    function test_dsrAllocation_depositUSDCToDSR() external {
+        // Start pranking with the USDC owner
+        vm.startPrank(ShutterGnosis);
+        // Stores the previous balance of the user
+        uint256 initialGnosisUSDCBalance = USDC.balanceOf(ShutterGnosis);
+        // Approve PSM to spend USDC {ERC20-approve}
+        USDC.approve(AuthGemJoin5, amount * decimalsUSDC);
+        // Check if allowance is set for USDC {ERC20-allowance}
+        assert(USDC.allowance(ShutterGnosis, AuthGemJoin5) == amount * decimalsUSDC);
+        // Convert USDC to DAI {DssPsm-sellGem}
+        DssPsm.sellGem(ShutterGnosis, amount * decimalsUSDC);
+        // Check if DAI balance was increased {ERC20-balanceOf}
+        assert(DAI.balanceOf(ShutterGnosis) == amount * decimalsDAI);
+        assert(USDC.balanceOf(ShutterGnosis) == initialGnosisUSDCBalance - amount * decimalsUSDC);
+        // Stores the previous balance of the user
+        uint256 balanceOfSavingsDaiBefore = SavingsDai.balanceOf(ShutterGnosis);
+        // Approve SavingsDai to spend DAI {ERC20-approve}
+        DAI.approve(address(SavingsDai), amount * decimalsDAI);
+        // Check if allowance is set for DAI {ERC20-allowance}
+        assert(DAI.allowance(ShutterGnosis, address(SavingsDai)) == amount * decimalsDAI);
+        // Preview the amount of shares that will be received {SavingsDai-previewDeposit}
+        uint256 sharesToBeReceived = SavingsDai.previewDeposit(amount * decimalsDAI);
+        // Deposit DAI to SavingsDai {SavingsDai-deposit}
+        uint256 sharesReceived = SavingsDai.deposit(amount * decimalsDAI, ShutterGnosis);
+        // Check if the amount of shares received is the same as the previewed amount {SavingsDai-deposit}
+        assert(sharesReceived == sharesToBeReceived);
+        // Check if the user's balance of shares was increased {SavingsDai-balanceOf}
+        assert(sharesReceived == SavingsDai.balanceOf(ShutterGnosis) - balanceOfSavingsDaiBefore);
+    }
 }
--- proposals/test-safe-gnosis/transfer.sol
+++ proposals/test-safe-gnosis/transfer.sol
@@ -7,21 +7,21 @@ import { Test } from "forge-std/src/Test.sol";
 import { console2 } from "forge-std/src/console2.sol";
 
 contract Context is Test {
-  /// @dev Recipient of token
-  address recipient = 0xfE1552DA65FAcAaC5B50b73CEDa4C993e16d4694;
+    /// @dev Recipient of token
+    address recipient = 0xfE1552DA65FAcAaC5B50b73CEDa4C993e16d4694;
 
-  // Amount of SHU to be sent to the contributor
-  uint256 amount = 5;
-  uint256 constant decimalsSHU = 10 ** 18;
+    // Amount of SHU to be sent to the contributor
+    uint256 amount = 5;
+    uint256 constant decimalsSHU = 10 ** 18;
 
-  /// @dev Shutter Token
-  IERC20 ShutterToken = IERC20(0x8CCd277Cc638E7e17F8100cE583cBcEf42007Dca);
+    /// @dev Shutter Token
+    IERC20 ShutterToken = IERC20(0x8CCd277Cc638E7e17F8100cE583cBcEf42007Dca);
 
-  /**
-   * @dev Prepares the transactions to be submitted in the proposal.
-   * @return transaction The transactions to be executed in the proposal.
-   */
-  function test_EncodeTransferCall() external view returns (bytes memory) {
-   return abi.encodeWithSelector(IERC20.transfer.selector, recipient, amount * decimalsSHU);
-  }
+    /**
+     * @dev Prepares the transactions to be submitted in the proposal.
+     * @return transaction The transactions to be executed in the proposal.
+     */
+    function test_EncodeTransferCall() external view returns (bytes memory) {
+        return abi.encodeWithSelector(IERC20.transfer.selector, recipient, amount * decimalsSHU);
+    }
 }
