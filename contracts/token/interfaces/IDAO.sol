7b15f24300d8d6714396dba412b65964ea1c0e3e
Alexandro T. Netto <alex.t.netto@gmail.com>
2024-10-08 16:33:26 -0300

    feat: add a common DAO interface

---
+++ contracts/token/interfaces/IDAO.sol
@@ -0,0 +1,8 @@
+interface IDAO {
+    function test_proposal() external;
+    function _selectFork() external;
+    function _proposer() external view returns (address);
+    function _voter() external view returns (address);
+    function _afterExecution() external;
+    function _generateCallData() external returns (address[] memory targets, uint256[] memory values, bytes[] memory calldatas);   
+}
